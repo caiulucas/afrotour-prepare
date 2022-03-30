14f0fe09012823f7a188df88fba324a9f48083c5
Caio Lucas <lienscarlet@gmail.com>
2022-03-30 17:26:24 -0300

    Preparing for class

---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+export function Content() {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Brasil</span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+        </div>
+      </main>
+    </div>
+  )
+}
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+import '../styles/sidebar.scss'
+
+export function SideBar() {
+  return (
+    <nav className="sidebar">
+      <span>AfroTour</span>
+    </nav>
+  )
+}
